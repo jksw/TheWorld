f51b5ea01bf46c26898f2ca415fc81cbf12d5aaa
asfdasfdsadfsafd <aaatofu@outlook.com>
2016-12-23 14:02:09 -0800

    Got web api working.  Be sure to use using Microsoft.AspNetCore.Mvc;  NOT aspNet.mvc

---
+++ src/TheWorld/Controllers/Api/TripsController.cs
@@ -0,0 +1,27 @@
+﻿
+//got 404 when accidentally did 'using ... aspet.mvc.
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWorld.Models;
+
+namespace TheWorld.Controllers.Api
+{
+  //[Route("api/[controller]")]
+  public class TripsController : Controller
+  {
+
+    //If you want to test with Postman tool, go to Project Proeprties  Debug -- uncheck Launch URL
+    //Use Postman in conj. with the Httpget attribute below
+
+    [HttpGet("api/trips")]
+    public IActionResult Get()
+    { 
+      return Ok(new Trip() { Name = "My Trip" });
+    }
+  }
+}
